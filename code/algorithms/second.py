893f461619f2941acb8124e45663d265ece11e63
Your Name <victorvanleuven@msn.com>
2022-01-28 14:21:17 +0100

    second

--- code/algorithms/second.py
+++ code/algorithms/second.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import grid
 import numpy as np
 from .helpers import check_max_value, is_almost_valid, list_compare, random, is_valid, list_remove
 
-class Test2_algorithm():    
+class Second():    
     def __init__(self, grid, netlist):
         
         self.netlist = netlist
@@ -25,24 +25,17 @@ class Test2_algorithm():
 
 
         probabillities = [1, 1, 1, 1, 95, 1]
-
-        directions = [0, 1, 2]
+        adjustments = [np.array((1, 0, 0)), np.array((-1, 0, 0)), np.array((0, 1, 0)),
+                        np.array((0, -1, 0)), np.array((0, 0, 1)), np.array((0, 0, -1))]
         
         forbidden_gates = set(self.gates.values()) - set(end)
 
-        for direction in directions:
+        for direction in range(3):
             adjustment = np.array((0, 0, 0))
             if step[direction] < end[direction]:
                 probabillities[0] += 1000
             elif step[direction] > end[direction]:
                 return adjustment
-
-        adjustments = []
-        for direction in range(3):
-            for plus_min in [-1, 1]:
-                adjustment = np.array(0, 0, 0)
-                adjustment[direction] = plus_min
-                adjustments.append[adjustment]
 
         adjustment = np.random.choice(adjustments, 6,replace=False, p=[1/100, 1/100, 1/100, 1/100, 95/100, 1/100])
 
